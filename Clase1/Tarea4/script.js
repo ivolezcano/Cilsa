5078d862fddb6800eef68e09b51e6b0fcd85f979
Ivan Lezcano <lezcamati@gmail.com>
2024-08-22 18:21:28 -0300

    Tarea completa

---
+++ Clase1/Tarea4/script.js
@@ -0,0 +1,8 @@
+document.getElementById('boton-original').addEventListener('click', function(){
+    document.body.classList.remove('alto-contraste')
+})
+
+document.getElementById('boton-alto-contraste').addEventListener('click', function(){
+    document.body.classList.add('alto-contraste')
+})
+
